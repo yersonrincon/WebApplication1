ee3afc93104c18730a32820cc6168e3fe17217fa
yersonrincon <yersonhernandez202@gmail.com>
2021-05-20 21:56:32 +0200

    cnsultando datos de la tabla

---
+++ WebApplication1/Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuario.ToList());
+            }
+        }
+
+    }
+}
+
