490533feb763928344dd3b88ee02a3f570dca6ab
yersonrincon <yersonhernandez202@gmail.com>
2021-05-20 23:23:57 +0200

    usuario controller

--- WebApplication1/Controllers/UsuarioController.cs
+++ WebApplication1/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
@@ -17,7 +18,53 @@ namespace WebApplication1.Controllers
                 return View(db.usuario.ToList());
             }
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(usuario usuario)
+        {
+
+            if (!ModelState.IsValid)
+
+                return View();
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuario.password = UsuarioController.HashSHA1(usuario.password);
+                    db.usuario.Add(usuario);
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+
+
+
+
+        }
+        public static string HashSHA1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
 
     }
 }
-
