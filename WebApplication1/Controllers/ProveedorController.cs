6ad8cb08b8ebdd293742129f9ed4bc5afc6049f5
yersonrincon <yersonhernandez202@gmail.com>
2021-06-19 20:43:29 -0500

    proyecto de inventario 2021 completo

--- WebApplication1/Controllers/ProveedorController.cs
+++ WebApplication1/Controllers/ProveedorController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
+using System.IO;
 using System.Web.Mvc;
 using WebApplication1.Models;
+using System.Web;
 
 namespace WebApplication1.Controllers
 {
@@ -16,6 +19,66 @@ namespace WebApplication1.Controllers
                 return View(db.proveedor.ToList());
             }
         }
+        public ActionResult uploadCSV()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            //string para guardar la ruta
+            string filePath = string.Empty;
+
+            //condicion para saber si llego el archivo
+            if (fileForm != null)
+            {
+                //ruta de la carpeta que gurdara el archivo
+                string path = Server.MapPath("~/Uploads/");
+
+                //condicion para saber si la ruta de la carpeta existe
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                //obtener el nombre del archivo
+                filePath = path + Path.GetFileName(fileForm.FileName);
+                //obtener la extension del archivo
+                string extension = Path.GetExtension(fileForm.FileName);
+
+                //guardar el archivo
+                fileForm.SaveAs(filePath);
+
+                string csvData = System.IO.File.ReadAllText(filePath);
+
+                foreach (string row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        var newProveedor = new proveedor
+                        {
+                            nombre = row.Split(';')[0],
+                            direccion = row.Split(';')[1],
+                            telefono = row.Split(';')[2],
+                            nombre_contacto = row.Split(';')[3],
+                        };
+
+                        using (var db = new inventario2021Entities())
+                        {
+                            db.proveedor.Add(newProveedor);
+                            db.SaveChanges();
+                        }
+                    }
+                }
+            }
+
+            return View();
+        }
+
+
+
+
+
         public static string NombreProveedor(int idProveedor)
         {
             using (var db = new inventario2021Entities())
@@ -120,4 +183,3 @@ namespace WebApplication1.Controllers
         }
     }
 }
-
